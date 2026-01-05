a4b5c1006661081ff636059095c3b8e6d8f04f28
Tarun Dewangan <tarundewangan666@gmail.com>
2026-01-05 21:24:09 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -67,7 +67,8 @@ app.use(cors({
   origin: allowedOrigins,
   credentials: true
 }));
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Routes
 app.use('/api/admin', adminRoutes);
